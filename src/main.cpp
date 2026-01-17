bb20fd79010e9706e6de7074bf19986b0b56f72f
MrZoom24 <jumberi.kilasonia24@gmail.com>
2026-01-18 02:14:51 +0400

    Add scanline progress indicator

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@ int main() {
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = 0; j < image_height; j++) {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; i++) {
             double r = double(i) / (image_width - 1);
             double g = double(j) / (image_height-1);
@@ -20,4 +21,5 @@ int main() {
         }
     }
 
+    std::clog << "\rDone.                 \n";
 }
